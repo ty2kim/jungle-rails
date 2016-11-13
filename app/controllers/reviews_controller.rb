1c374853eec305c30924dac7179ccabd508ed741
TAE YOUNG KIM <ty2kim@gmail.com>
2016-11-13 17:44:39 +0000

    step11

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,23 @@
 # class ReviewsController
 class ReviewsController < ApplicationController
   def create
-    raise "Yay, I'm here!"
+    @product = Product.find params[:product_id]
+    @review = @product.reviews.new(review_params)
+    @review.user = current_user
+
+    if @review.save
+      redirect_to :products, notice: 'Review created!'
+    else
+      render @product
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating
+    )
   end
 end
