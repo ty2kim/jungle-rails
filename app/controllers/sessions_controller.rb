864d875d5fcc00b2a979f31132a09e061a854d87
TAE YOUNG KIM <ty2kim@gmail.com>
2016-11-15 23:19:26 +0000

    flash messages added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
+      flash[:notice] = 'Incorrect email or password'
       redirect_to '/login'
     end
     # user = User.find_by_email(params[:email])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
       session[:user_id] = user.id
       redirect_to '/'
     else
+      flash[:notice] = "#{user.errors.full_messages}"
       redirect_to '/signup'
     end
   end
